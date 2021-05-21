596894730364c0d507e98d47cbe0fec85bab65bd
gauravgit <gs244330@gmail.com>
2021-05-21 12:58:44 +0530

    react project on Todo list

--- src/App.js
+++ src/App.js
@@ -1,45 +1,82 @@
-
 import './App.css';
-import Header from"./MyComponents/Header";
-import {Footer} from "./MyComponents/Footer";
-import {Todos} from "./MyComponents/Todos";
-import React, { useState } from 'react';
+import Header from "./MyComponents/Header";
+import { Todos } from "./MyComponents/Todos";
+import { Footer } from "./MyComponents/Footer";
+import { AddTodo } from "./MyComponents/AddTodo";
+import { About } from "./MyComponents/About";
+import React, { useState, useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
 
 function App() {
-  const onDelete=(todo)=>{
-    console.log("I am obdelet of todo",todo);
-    setTodos(todos.filter((e)=>{
-      return e!==todo;
-    }))
+  let initTodo;
+  if (localStorage.getItem("todos") === null) {
+    initTodo = [];
   }
-  
-  const [todos,setTodos]=useState([
-  {
-    sno: 1,
-    title: "go to market",
-    desc:"go to market for work"
-  },
-  {
-    sno: 2,
-    title: "go to gym",
-    desc:"go to gym for workout"
-  },
-  {
-    sno: 3,
-    title: "go to work",
-    desc:"go to work"
+  else {
+    initTodo = JSON.parse(localStorage.getItem("todos"));
   }
-  ])
-  return (
-    <>
-  
-      <Header  searchBar={false} title="My Todos List"/>
-      <Todos todos={todos} onDelete={onDelete}/>
-    
-      <Footer/>
-      
+
+
+  const onDelete = (todo) => {
+    console.log("I am ondelete of todo", todo);
+    // Deleting this way in react does not work
+    // let index = todos.indexOf(todo);
+    // todos.splice(index, 1);
+
+    setTodos(todos.filter((e) => {
+      return e !== todo;
+    }));
+    console.log("deleted", todos)
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }
+
+  const addTodo = (title, desc) => {
+    console.log("I am adding this todo", title, desc)
+    let sno;
+    if (todos.length === 0) {
+      sno = 0;
+    }
+    else {
+      sno = todos[todos.length - 1].sno + 1;
+    }
+    const myTodo = {
+      sno: sno,
+      title: title,
+      desc: desc,
+    }
+    setTodos([...todos, myTodo]);
+    console.log(myTodo);
+  }
+
+  const [todos, setTodos] = useState(initTodo);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos])
+
+  return ( 
+    <> 
+    <Router>
+      <Header title="My Todos List" searchBar={false} /> 
+      <Switch>
+          <Route exact path="/" render={()=>{
+            return(
+            <>
+            <AddTodo addTodo={addTodo} />
+            <Todos todos={todos} onDelete={onDelete} /> 
+            </>)
+          }}> 
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route> 
+        </Switch> 
+      <Footer />
+    </Router>
     </>
-    
   );
 }
 
--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -1,18 +1,21 @@
 import React from 'react'
-import { Todo } from './Todo'
+import {TodoItem} from "./TodoItem";
 
 export const Todos = (props) => {
+    let myStyle = {
+        minHeight: "70vh",
+        margin: "40px auto"
+    }
     return (
-        <div className="container">
-            <h3 className="text-center my-3">Todo List</h3>
-            {props.todos.length===0? "no todos to display":
+        <div className="container" style={myStyle}>
+            <h3 className="my-3">Todos List</h3>
+            {props.todos.length===0? "No Todos to display":  
             props.todos.map((todo)=>{
-                return (<Todo todo={todo} key={todo.sno} onDelete={props.onDelete}/>)
+                console.log(todo.sno);
+                return (<TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}/>   
+                )
             })
-        }
-            
-          
-            
-        </div> 
+              } 
+        </div>
     )
 }
